210c4c9555e932cfd27e1df62d6944bea4b8428e
Evgenii T <chasej18@inbox.ru>
2020-06-14 11:01:48 +0900

    Added a 'Makefile' and a function 'welcome_user'

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -2,7 +2,10 @@
 
 # -*- coding: utf-8 -*-
 
+from brain_games import cli
+
 def main():
-    print('Welcome to the Brain Games!')
+    print('Welcome to the Brain Games!/n')
+    cli.welcome_user()
 if __name__ == '__main__':
      main()
